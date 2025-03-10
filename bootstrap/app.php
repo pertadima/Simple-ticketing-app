7d47307deb769d202d29f3d5d39d7eea83d80e25
irfan.pertadima <pertadimairfan@gmail.com>
2025-03-10 10:48:13 +0700

    add exception handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,11 @@
 <?php
 
+use DanHarrin\LivewireRateLimiting\Exceptions\TooManyRequestsException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +18,27 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->renderable(function (NotFoundHttpException $e, $request) {
+            if ($request->json()) {
+                return response()->json(['errors' => [
+                    'error_code' => 404,
+                    'title' => 'Not Found',
+                    'message' => 'Resource not found'
+                ]], 404);
+            }
+
+            throw $e;
+        });
+
+        $exceptions->renderable(function (TooManyRequestsHttpException $e, $request) {
+            if ($request->json()) {
+                return response()->json(['errors' => [
+                    'error_code' => 429,
+                    'title' => 'Too many requests',
+                    'message' => 'Too many requests, try again later'
+                ]], 429);
+            }
+
+            throw $e;
+        });
     })->create();
